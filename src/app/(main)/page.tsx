a6c6aab236e88161ad9b29d5476c762b39fbea9a
Faiznhmd <fiaznhmd.0624@gmail.com>
2024-10-10 20:06:31 +0530

    style changes

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -2,7 +2,7 @@ import { AddVideo } from './components/addVideo';
 
 export default function Home() {
   return (
-    <div className=" h-screen w-screen flex flex-col items-center">
+    <div className=" h-screen w-screen flex justify-center items-center ">
       <AddVideo />
     </div>
   );
